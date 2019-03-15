8d38728cd6378cf28bc9b1027fc7e4dba86101f1
MartoInholland <42969166+martoinholland@users.noreply.github.com>
2019-03-15 21:51:06 +0100

    almost final

--- Someren1819/SomerenModel/Student.cs
+++ Someren1819/SomerenModel/Student.cs
@@ -12,13 +12,5 @@ namespace SomerenModel
         public String LastName { get; set; }
         public int Number { get; set; } // StudentNumber, e.g. 474791
         public DateTime BirthDate { get; set;  }
-
-        //please work //new change
-
-        //yasss it works
-        ////newwwwww
-
-
-        //testing github
     }
 }
